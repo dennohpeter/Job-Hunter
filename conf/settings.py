ec4426f57b5680ba4233ed553e0763d09ad28a8a
Dennis Peterson <denispeterson96@gmail.com>
2019-10-08 00:52:28 +0300

    updated default db to use dj_database_url

--- conf/settings.py
+++ conf/settings.py
@@ -1,6 +1,8 @@
 import os
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,7 +81,8 @@ DATABASES = {
         'PORT': '',
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
